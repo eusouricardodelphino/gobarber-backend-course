59007752eee514b74c6fe307dc00ec8f7129e456
Ricardo Delphino <radelphino@gmail.com>
2021-10-18 13:03:32 -0300

    Go Barber backend init

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '36bccd36fbc67cd47ab525ec93b950f1',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import appointmentsRouter from './appointments.routes';
+import sessionsRouter from './sessions.routes';
+import usersRouter from './users.routes';
+
+const routes = Router();
+
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello Go Stack' }),
+);
+
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+
+export default routes;
